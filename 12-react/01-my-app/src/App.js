f7f16fbddc7a0ccd839638a3ea25671b595f85b6
Your Name <you@example.com>
2021-10-05 23:33:19 +0300

    v30

---
+++ 12-react/01-my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hello from "./components/hello/Hello";
+import Test from "./components/test/Test";
+
+function App() {
+  return (
+    <div>
+      Merhaba Dünya
+      <Hello />
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ 12-react/01-my-app/src/components/test/Test.js
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>Bu bir test componenti dir.</div>;
+};
+
+export default Test;
