fd8d84b6540d684ab5f39d35cfebe29acf540753
Your Name <you@example.com>
2021-10-07 22:23:02 +0300

    v35

--- 12-react/01-my-app/src/App.js
+++ 12-react/01-my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Clock1 from "./components/clock1/Clock1";
 import Hello from "./components/hello/Hello";
 import Jsx from "./components/jsx/Jsx";
+import Props from "./components/props/Props";
 import ExternalStyle from "./components/style/ExternalStyle";
 import InlineStyle from "./components/style/InlineStyle";
 import InternalStyle from "./components/style/InternalStyle";
@@ -20,8 +21,15 @@ function App() {
       <InlineStyle />
       <InternalStyle />
       <ExternalStyle />
- */}
+ 
       <Clock1 />
+
+      */}
+
+      <Props ad="Ziya" soyad="Sarı" />
+      <Props ad="Ali" soyad="Kara" />
+      <Props ad="Ayşe" soyad="Pembe" />
+      <Props ad="Hakan" soyad="Mavi" />
     </div>
   );
 }
